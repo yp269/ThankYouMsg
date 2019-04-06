7ff0fac38909cb97b3a8dfa11562a4b6465a141e
Yi <yp269@njit.edu>
2019-04-06 19:58:06 -0400

    Task:added form

--- resources/views/Pages/contact.blade.php
+++ resources/views/Pages/contact.blade.php
@@ -7,7 +7,8 @@
     <p class="lead">Please use this form to contact the site owner.</p>
 
 
-    <form>
+    <form action="/contact" method="post">
+        @csrf
         <div class="form-group">
             <label for="email">Email address</label>
             <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
