6f688c854ca1e41b3d267b8f332dd38348e8c5ae
Yi <yp269@njit.edu>
2019-04-06 18:51:53 -0400

    Task:added jumbotron

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+@include('includes.head')
+
+<body>
+    @include('includes.menu')
+
+    <main role="main">
+
+    @yield('content')
+
+    </main>
+
+@include('includes.jsfooter')
+@include('includes.footer')
+
+</body>
+</html>
