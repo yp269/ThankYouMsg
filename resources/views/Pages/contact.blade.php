7717c54296bacc1319b0fcba9b3a3266eeb27b9b
Yi <yp269@njit.edu>
2019-04-06 19:35:34 -0400

    Task:added contact page

---
+++ resources/views/Pages/contact.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.default')
+
+@section('content')
+
+
+    <h1>Contact</h1>
+    <p class="lead">Please use this form to contact the site owner.</p>
+
+
+    <form>
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+        </div>
+
+        <div class="form-group">
+            <label for="body">Message</label>
+            <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary mb-2">Submit</button>
+    </form>
+
+@endsection
+
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,16 +4,24 @@
 @include('includes.head')
 
 <body>
-    @include('includes.menu')
+
+<div class="container">
+
+    <header>
+        @include('includes.menu')
+    </header>
 
     <main role="main">
 
-    @yield('content')
+        @yield('content')
 
     </main>
 
-@include('includes.jsfooter')
-@include('includes.footer')
+    @include('includes.jsfooter')
 
+    <footer>
+        @include('includes.footer')
+    </footer>
+</div>
 </body>
 </html>
